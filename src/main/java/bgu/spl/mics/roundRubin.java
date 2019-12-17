4c570a0c784f3d62a50766ae82ce37039eee1194
nimrodberman <33838639+nimrodberman@users.noreply.github.com>
2019-12-17 20:04:08 +0200

    update2

---
+++ src/main/java/bgu/spl/mics/roundRubin.java
@@ -0,0 +1,45 @@
+package bgu.spl.mics;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class roundRubin {
+
+    private ArrayList<Subscriber> list;
+    private AtomicInteger i;
+
+    public roundRubin(ArrayList<Subscriber> List, AtomicInteger x){
+        this.list = List;
+        this.i = x;
+    }
+
+    public ArrayList<Subscriber> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Subscriber> List) {
+        this.list = List;
+    }
+
+    public AtomicInteger getCounter() {
+        return i;
+    }
+
+
+    public void setI(int x) {
+        this.i.set(x);
+    }
+
+
+    public void setItonext() {
+        this.i.incrementAndGet();
+    }
+
+
+    public void setItozero() {
+        this.i.set(0);
+    }
+
+
+}
